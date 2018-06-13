edb75d5459e9fd66a65209a5c14b5cd075d73f36
cplant <calebplant@aggienetwork.com>
2018-06-13 16:55:41 -0500

    Initial version. Functioning battle between 2 entities. Need to add navigation now, then equipment.

--- src/game.py
+++ src/game.py
@@ -7,13 +7,4 @@ from src.battle_flow.battle import *
 MAIN
 ==============================='''
 if __name__ == "__main__":
-    x = Player(name="Mike")
-    y = Monster(name="Azmodeus",strength=4)
-    y.phys_def = 10
-    y.mag_def = 10
-    y.phys_attack = 10
-
-    die = Dice()
-    wpn = Weapon()
-
-    battle(x, y, die)
+    pass
